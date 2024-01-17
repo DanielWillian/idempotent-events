15d7efe8a78cf90a04e1105eae780b9efcb5f47e
Daniel Willian <d.willian_sc@hotmail.com>
2024-01-17 10:17:13 -0300

    Use idempotency repository on logging listener

--- domain/src/main/java/org/domain/events/domain/LoggingListener.java
+++ domain/src/main/java/org/domain/events/domain/LoggingListener.java
@@ -1,11 +1,27 @@
 package org.domain.events.domain;
 
+import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import org.domain.events.domain.idempotency.IdempotencyId;
+import org.domain.events.domain.idempotency.IdempotencyRepository;
 
 @Slf4j
+@Singleton
 public class LoggingListener implements EventListener<DomainEvent> {
+    @Inject
+    private IdempotencyRepository idempotencyRepository;
+
     @Override
     public void process(DomainEvent event) {
+        IdempotencyId idempotencyId = IdempotencyId.of(event.getEventId().getId());
+        if (idempotencyRepository.getIdempotency(idempotencyId).isPresent()) {
+            log.info("Already processed event: {}", event);
+            return;
+        }
+
         log.info("Processing event: {}", event);
+
+        idempotencyRepository.saveIdempotency(idempotencyId);
     }
 }
